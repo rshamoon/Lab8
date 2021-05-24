82e6a198d1bdeaf297a8a8e4a69e75f5e3c50e44
Ryan Shamoon <rshamoon@ucsd.edu>
2021-05-23 20:03:26 -0700

    Finished!
    Co-authored-by: bgt002 <bgt002@users.noreply.github.com>
    Co-authored-by: asya-balas <asya-balas@users.noreply.github.com>

--- README.md
+++ README.md
@@ -8,7 +8,6 @@
 ## Check your understanding q's (FILL OUT)
 1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)
     - A
-
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
    - No, we would not use a unit test. This is because the "message" feaiture is not a small individual component, but instead a complex operation that consists of smaller components. The "message" feature involves many different parts of the code interacting, which is not what unit tests are for.
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
@@ -16,4 +15,9 @@
 4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
     - If "headless" is true, the it will run tests without opening the browser UI. This is different than when "headless" is false, which causes the Chromium browser to open.
 5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+    beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500');
+    await page.waitForTimeout(500);
+    await page.click('img');
+  });
 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -4,7 +4,7 @@ describe('Basic user flow for SPA ', () => {
     await page.waitForTimeout(500);
   });
 
-  // test 1 is given
+  //test 1 is given
   it('Test1: Initial Home Page - Check for 10 Journal Entries', async () => {
     const numEntries = await page.$$eval('journal-entry', (entries) => {
       return entries.length;
@@ -12,7 +12,7 @@ describe('Basic user flow for SPA ', () => {
     expect(numEntries).toBe(10);
   });
 
-  // test 2 is given
+  //test 2 is given
   it('Test2: Make sure <journal-entry> elements are populated', async () => {
     let allArePopulated = true;
     let data, plainValue;
@@ -55,6 +55,17 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
+    let allContent = true;
+    let data, plainValue;
+    const entries = await page.$$('journal-entry');
+    data = await entries[0].getProperty('entry');
+    plainValue = await data.jsonValue();
+    if (plainValue.title != 'You like jazz?') { allContent = false; }
+    if (plainValue.date != '4/25/2021') { allContent = false; }
+    if (plainValue.content != "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.") { allContent = false; }
+    if (plainValue.image.src != 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455'){ allContent = false; }
+    if (plainValue.image.alt != 'bee with sunglasses'){ allContent = false; }
+    expect(allContent).toBe(true);
     
   }, 10000);
 
@@ -62,53 +73,139 @@ describe('Basic user flow for SPA ', () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
     const className = await page.$eval('body', el => el.className);
     expect(className).toBe('single-entry');
-
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
-    
+    await page.click('img');
+    expect(page.url().includes('/#settings')).toBe(true);
   });
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    const header = await page.$eval('body > header > h1', el => el.textContent);
+    expect(header).toBe('Settings');
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    const className = await page.$eval('body', el => el.className);
+    expect(className).toBe('settings');
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
-
+    await page.goBack();
+    expect(page.url().includes('/#entry1')).toBe(true);
   });
 
-  // define and implement test11: Clicking the back button once should bring the user back to the home page
+  it('Test11: Clicking the back button, new URL should be /', async() => {
+    // define and implement test11: Clicking the back button once should bring the user back to the home page
+      await page.goBack();
+      expect(page.url().includes('/')).toBe(true);
+  });
 
+  it('Test12: On Home Page, the heading should be "Journal Entries"', async() => {
+  // define and implement test12: When the user is on the homepage, the header title should be “Journal Entries”
+    const header = await page.$eval('body > header > h1', el => el.textContent);
+    expect(header).toBe('Journal Entries');
+  });
 
-  // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-
-
+  it('Test13: On the home page, the body element should not have any class attributes', async() => {
   // define and implement test13: On the home page the <body> element should not have any class attribute 
+    const className = await page.$eval('body', el => el.className);
+    expect(className).toBe('');
+  });
 
-
+  it('Test14: On the Second Entry, URL should include /#entry2', async() => {
   // define and implement test14: Verify the url is correct when clicking on the second entry
+    await page.$$eval('journal-entry', (entries) => {
+      entries[1].click();
+    });
+    expect(page.url().includes('/#entry2')).toBe(true);  
+  });
 
-
+  it('Test15: On the Second Entry, Title should be Entry 2', async() => {
   // define and implement test15: Verify the title is current when clicking on the second entry
-
-
+    const header = await page.$eval('body > header > h1', el => el.textContent);
+    expect(header).toBe('Entry 2');
+  });
+    
+  it('Test16: On second Entry page - checking <entry-page> contents', async() => {
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+  /*
+     implement test16: Clicking on the second journal entry should contain the following contents: 
+        { 
+          title: 'Run, Forrest! Run',
+          date: '4/26/2021',
+          content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+          image: {
+            src: 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg',
+            alt: 'forrest running'
+        }
+       }
+    */
+    let entry2Content = true;
+    let data, plainValue;
+    const entries = await page.$$('journal-entry');
+    data = await entries[1].getProperty('entry');
+    plainValue = await data.jsonValue();
+    if (plainValue.title != 'Run, Forrest! Run!') { entry2Content = false; }
+    if (plainValue.date != '4/26/2021') { entry2Content = false; }
+    if (plainValue.content != "Mama always said life was like a box of chocolates. You never know what you're gonna get.") { entry2Content = false; }
+    if (plainValue.image.src != 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg'){ entry2Content = false; }
+    if (plainValue.image.alt != 'forrest running'){ entry2Content = false; }
+    expect(entry2Content).toBe(true);
+    
+  });
 
-
-  // create your own test 17
-
-  // create your own test 18
-
-  // create your own test 19
-
-  // create your own test 20
+  it('Test17: Clicking on the back and then forward button, the URL should contain /#entry2', async() => {
+  // create your own test 17: Verify the forward button works properly after hitting the back button once.
+    await page.goBack();
+    await page.goForward();
+    expect(page.url().includes('/#entry2')).toBe(true);
+  });
   
+  it('Test18: On the Ninth Entry, URL should include /#entry9', async() => {
+  // create your own test 18: Verify the url is correct when clicking on the ninth entry
+    await page.goBack();
+    await page.$$eval('journal-entry', (entries) => {
+      entries[8].click();
+    });
+    expect(page.url().includes('/#entry9')).toBe(true); 
+  });
+  
+  it('Test19: On the Ninth Entry, Title should be Entry 9', async() => {
+  // create your own test 19: Verify the title is current when clicking on the ninth entry
+    const header = await page.$eval('body > header > h1', el => el.textContent);
+    expect(header).toBe('Entry 9');
+  
+  });
+
+  it('Test20: On Ninth Entry page - checking <entry-page> contents', async() => {
+  // create your own test 20: Verify the entry page contents is correct when clicking on the ninth entry
+  /*
+     implement test16: Clicking on the ninth journal entry should contain the following contents: 
+      { 
+          title: 'Keep moving forward',
+          date: '5/3/2021',
+          content: "Everyone will tell you to let it go and move on, but don't! Instead, let it fester and boil inside of you! Take these feelings and lock them away. Let them fuel your actions. Let hate be your ally, and you will be capable of wonderful, horrid things. Heed my words, Goob: don't let it go.",
+          image: {
+            src: 'https://i.pinimg.com/originals/7b/13/82/7b13822ce506f9d3fcb7707c0f6e72c3.png',
+            alt: "mr. steak you're my only friend"
+          },
+      }
+  */
+    let entry9Content = true;
+    let data, plainValue;
+    const entries = await page.$$('journal-entry');
+    data = await entries[8].getProperty('entry');
+    plainValue = await data.jsonValue();
+    if (plainValue.title != 'Keep moving forward') { entry9Content = false; }
+    if (plainValue.date != '5/3/2021') { entry9Content = false; }
+    if (plainValue.content != "Everyone will tell you to let it go and move on, but don't! Instead, let it fester and boil inside of you! Take these feelings and lock them away. Let them fuel your actions. Let hate be your ally, and you will be capable of wonderful, horrid things. Heed my words, Goob: don't let it go.") { entry9Content = false; }
+    if (plainValue.image.src != 'https://i.pinimg.com/originals/7b/13/82/7b13822ce506f9d3fcb7707c0f6e72c3.png'){ entry9Content = false; }
+    if (plainValue.image.alt != "mr. steak you're my only friend") { entry9Content = false; } 
+    expect(entry9Content).toBe(true);
+  });
 });
